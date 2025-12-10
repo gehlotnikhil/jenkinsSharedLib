b376cfc74855165d9b5bfa01f94d159dd6faa01a
Nikhil Gehlot <111073969+gehlotnikhil@users.noreply.github.com>
2025-12-10 11:59:48 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String DockerHubUser,String ProjectName, String ImageTag,String Location){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ${Location}"
 }
